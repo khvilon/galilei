e0898d6c84a6f1a90a137ece9066d06c4301451e
volhv <tusaty@bk.ru>
2022-11-16 07:51:46 +0000

    implement team invitations

--- ds/tools/ml_worker.py
+++ ds/tools/ml_worker.py
@@ -99,7 +99,7 @@ import hnswlib
 def combine(vecs, norm='l2'):
     rs = torch.zeros(768)
     n = 0
-    for v, w in vecs.items():
+    for v, w in vecs:
         if not v is None and v == v:
             rs += word2vec(v) * w
             n  += w
@@ -114,6 +114,12 @@ def combine(vecs, norm='l2'):
 def get_usr_liked(uid, con):
     likes = pd.read_sql_query(
         "select * from ideas ii join likes ll on ii.uuid=ll.idea_uuid where ll.author_uuid='%s' order by ll.created_at desc" % uid , con=con)
+    return likes.drop_duplicates()
+
+def get_liked(idea_uid, con):
+    likes = pd.read_sql_query(
+        "select ll.author_uuid, name, description, is_positive, category_uuid "\
+        "from ideas ii join likes ll on ii.uuid=ll.idea_uuid where ll.idea_uuid='%s' order by ll.created_at desc" % idea_uid , con=con)
     return likes.drop_duplicates()
 
 
@@ -149,18 +155,26 @@ class FeedRefresher:
     def put_new_like(self, user_uuid, item_uuid, is_positive):
         uuid4s = str(uuid.uuid4())
         self.pm.query(
-            f"INSERT into {self.table} (uuid, author_uuid, idea_uuid,"\
-            f" is_positive, created_at, updated_at) values ('{uuid4s}', '{user_uuid}', '{item_uuid}', {is_positive}, NOW(), NOW())")         
+            f"INSERT into likes (uuid, author_uuid, idea_uuid,"\
+            f" is_positive, created_at, updated_at) values ('{uuid4s}', '{user_uuid}', '{item_uuid}', {is_positive}, NOW(), NOW())")  
+        
+        
+    def put_new_team_request(self, user_uuid, item_uuid, level):
+        uuid4s = str(uuid.uuid4())
+        self.pm.query(
+            f"INSERT into team_requests_advices (uuid, user_uuid, idea_uuid,"\
+            f" level, created_at, updated_at) values ('{uuid4s}', '{user_uuid}', '{item_uuid}', {level}, NOW(), NOW())")         
+            
     
 
 class ML_Ideas_Feeder_Baseline():
     
     
-    def __init__(self, conn, n_max_items=10000):
+    def __init__(self, conn, n_max_items=100000):
         self.conn = conn
         # av. spaces: l2, cosine or ip
         self.idx = hnswlib.Index(space = 'cosine', dim = 768) 
-        self.idx.init_index(max_elements = n_max_items, ef_construction = 200, M = 16)
+        self.idx.init_index(max_elements = n_max_items, ef_construction = 300, M = 32)
         self.uuids = {}
         self.feeder = FeedRefresher(conn, 'likes_advices', 'user_uuid', 'idea_uuid')
         self.categories = pd.read_sql("select uuid, name from idea_categories", con=conn)
@@ -171,7 +185,7 @@ class ML_Ideas_Feeder_Baseline():
             return self.categories.loc[cat_id, "name"]
         
     def __update_ideas_index(self, idea):
-        vec = combine({idea["name"]:1, idea.description:1, self.category_to_text(idea.category_uuid):5})
+        vec = combine([(idea["name"], 1), (idea.description,1), (self.category_to_text(idea.category_uuid),5)])
         self.uuids[self.idx.element_count] = idea["uuid"]
         self.idx.add_items(vec)
     
@@ -192,12 +206,10 @@ class ML_Ideas_Feeder_Baseline():
     #             self.feeder.put_new_item(author_uuid, idea_uuid, 1 - d)
     
     def __handle_user_like(self, like, top_k=5):
-        print("Got like: ", like)
-        # get_usr_by_uid(like.author_uuid)
         liked_ideas = get_usr_liked(like.author_uuid, con=self.conn)
         candidates = []
         for _, idea  in liked_ideas.head(top_k).iterrows():
-            query = combine({idea["name"]:1, idea.description:1, self.category_to_text(idea.category_uuid):5})
+            query = combine([(idea["name"], 1), (idea.description,1), (self.category_to_text(idea.category_uuid),5)])
             labels, distances = self.idx.knn_query(query, 10)
             labels = [self.uuids[s] for s in labels.reshape(-1)]
             sign = 1 if idea.is_positive else -1
@@ -206,23 +218,50 @@ class ML_Ideas_Feeder_Baseline():
 
         candidates = pd.concat(candidates)
         candidates = candidates.groupby("idea_uuid")["level"].mean()
-        self.feeder.clear_for_user(like.author_uuid)
+        # self.feeder.clear_for_user(like.author_uuid)
         for idea_uuid, level in candidates.iteritems():
             self.feeder.put_new_item(like.author_uuid, idea_uuid, level * 100)
         
         
-    def start(self):
+    def __notify_on_new_ideas(self, idea, top_k=50, top_users=5):
+        query = combine([(idea["name"], 1), (idea.description,1), (self.category_to_text(idea.category_uuid),5)])
+        labels, distances = self.idx.knn_query(query, top_k)
+        labels = [self.uuids[s] for s in labels.reshape(-1)]
         
+        variants = []
+        for label in labels:
+            ideas = get_liked(label, con=self.conn)
+            variants += [ideas]
+            
+        users = pd.concat(variants)
+        users["is_positive"] = users["is_positive"].fillna(0.).astype(int)
+        
+        if users.empty: return
+        
+        levels = users.groupby("author_uuid")["is_positive"].mean().sort_values(ascending=False)
+        levels = levels.head(5)
+        for uuid, level in levels.iteritems():
+            print("notifying: ", idea.uuid, uuid, level * 100)
+            self.feeder.put_new_team_request(uuid, idea.uuid, level)
+            
+        
+    def start(self):
+        print("starting")
         
         self.ideas_tracker = DBTracker(self.conn, "ideas", "uuid", "updated_at")
         self.ideas_tracker.reset()
         self.ideas_tracker.add_handler(self.__update_ideas_index)
         self.ideas_tracker.attach()
+        self.ideas_tracker.check()
+        print("loaded")
+        self.ideas_tracker.add_handler(self.__notify_on_new_ideas)
+        
         
         self.likes_tracker = DBTracker(self.conn, "likes", "uuid", "updated_at")
         self.likes_tracker.attach()
         self.likes_tracker.add_handler(self.__handle_user_like)
         
+        print("working")
         while True: 
             sleep(2)
             self.ideas_tracker.check()
